c9d2aea532067f017f4392a2a15dd1c19e13cc6d
vasco sousa <vasco.alex.sousa@gmail.com>
2023-02-27 13:14:19 +0000

    Products Controller - first attempt

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,39 @@
+class ProductsController < ApplicationController
+  def index
+    if params[:search]
+      @products = Product.where('name LIKE ?', "%#{params[:search]}%")
+    else
+      @products = Product.all
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+end
