e5d08ef17288de69ada6e66aee3a47f9daab6c91
vasco sousa <vasco.alex.sousa@gmail.com>
2023-03-03 13:48:20 +0000

    Product Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_111522) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_103415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,6 +104,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_111522) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -127,4 +129,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_111522) do
   add_foreign_key "bookings", "products"
   add_foreign_key "bookings", "users"
   add_foreign_key "products", "users"
+  add_foreign_key "reviews", "bookings"
 end
